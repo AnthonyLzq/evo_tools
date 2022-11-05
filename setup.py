0b6ded2a59b1445d143c4647595500d180488f5b
AnthonyLzq <sluzquinosa@uni.pe>
2022-11-05 09:53:05 -0500

    fix: __init__.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2.1.4'
+VERSION = '2.1.5'
 DESCRIPTION = 'Evolutionary programming tools'
 LONG_DESCRIPTION = 'A package that allows you to implement evolutionary algorithms.'
 
