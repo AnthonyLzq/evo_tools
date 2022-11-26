0f7a72fe4161ba8fad51f2e47bbf4a857e3626cf
AnthonyLzq <sluzquinosa@uni.pe>
2022-11-26 14:49:50 -0500

    feat: updated version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2.1.5'
+VERSION = '2.2.0'
 DESCRIPTION = 'Evolutionary programming tools'
 LONG_DESCRIPTION = 'A package that allows you to implement evolutionary algorithms.'
 
